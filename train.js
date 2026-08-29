192aa3e0ee4f2d39fd8fb44b63584ef082a8d55e
Muhammadumar <memoriessaved1919@gmail.com>
2026-08-29 19:13:44 +0900

    feat: train callback hamda async function

--- train.js
+++ train.js
@@ -1,3 +1,39 @@
+// console.log ("Jack Ma maslahtlari");
+// const list = [
+//     "yaxshi talaba boling", // 0-20
+//     "togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "uzingizga ishlashni boshlang", // 30-40
+//     "siz kuchliy bolgan narsalarni qiling", // 40-50
+//     "yoshlarga invertitsiya qiling", // 50-60
+//     "endi dam oling foydasi yoq", // 60
+// ];
+
+// function maslahatBering (a, callback) {
+//     if (typeof a !== 'number') callback("insert a number",null);
+//     else if (a <= 20) callback(null, list[0]);
+//     else if (a > 20 && a <= 30) callback (null, list[1]);
+//     else if (a > 30 && a <= 40) callback (null, list[2]);
+//     else if (a > 40 && a <= 50) callback (null, list[3]);
+//     else if (a > 50 && a <= 60) callback (null, list[4]);
+//     else {
+//         setTimeout (function () {
+//             callback(null, list[5]);
+//         }, 5000)
+        
+//     } 
+// }
+
+// // callback - (parametr) siftida functioni ishga tushadi
+// console.log('passed here 0');
+// maslahatBering(65 , (err, data) => {
+//     if (err) console.log ("ERROR:", err)
+//         else console.log ("Javob:",data)
+// });
+// console.log('passed here 1')
+
+
+// ASYNC function
+
 console.log ("Jack Ma maslahtlari");
 const list = [
     "yaxshi talaba boling", // 0-20
@@ -8,6 +44,57 @@ const list = [
     "endi dam oling foydasi yoq", // 60
 ];
 
+// setInterval asynchronousda ishlamaydi.
+/* async function maslahatBering (a) {
+    if (typeof a !== 'number') throw new Error("insert a number");
+    else if (a <= 20) return (list[0]);
+    else if (a > 20 && a <= 30) return (list[1]);
+    else if (a > 30 && a <= 40) return (list[2]);
+    else if (a > 40 && a <= 50) return (list[3]);
+    else if (a > 50 && a <= 60) return (list[4]);
+    else {
+        return new Promise ((resolve, reject) => {
+            setTimeout(() => {
+                resolve(list[5])
+            }, 5000 )
+        });  
+        // setTimeout (function () {
+        //     return (list[5]);
+        // }, 5000)
+        
+    } 
+} */
+
+// then - catch 
+// Bir vaqtni ozida 2 ta javobni chiqarmoqchi bolsam noqulay bo'lib (piramida)bop ketadi.
+
+
+// console.log('passed here 0');
+// maslahatBering(5)
+// .then((data) => {
+//     console.log('javob:',data)
+// })
+// .catch((err) => {
+//     console.log('ERROR:', err);
+// })
+// console.log('passed here 1');
+
+// Asynchronous functionni call qismida ham ishlatamiz
+// Bunda biz async va awaitdan foydalanamiz
+
+
+// async await qismda ketma ketlikk amal qiladi (1-javob, 2-javob,3-javob).
+/*async function run() {
+    let javob = await maslahatBering(22);
+    console.log(javob);
+    javob = await maslahatBering(80);
+    console.log(javob);
+} 
+run()*/
+
+
+// setIntervalni callbackda ishlaydi
+
 function maslahatBering (a, callback) {
     if (typeof a !== 'number') callback("insert a number",null);
     else if (a <= 20) callback(null, list[0]);
@@ -16,14 +103,13 @@ function maslahatBering (a, callback) {
     else if (a > 40 && a <= 50) callback (null, list[3]);
     else if (a > 50 && a <= 60) callback (null, list[4]);
     else {
-        setTimeout (function () {
+        setInterval ( function ()  {
             callback(null, list[5]);
-        }, 5000)
+        }, 1000)
         
     } 
 }
 
-// callback - (parametr) siftida functioni ishga tushadi
 console.log('passed here 0');
 maslahatBering(65 , (err, data) => {
     if (err) console.log ("ERROR:", err)
