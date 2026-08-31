1ae8186afeeb2b21b42a130477b727a1726badd0
Muhammadumar <memoriessaved1919@gmail.com>
2026-09-01 00:44:10 +0900

    feat: B-TASK bajarldi

--- train.js
+++ train.js
@@ -1,3 +1,112 @@
+// B-TASK
+console.log("B-TASK Javoblari")
+function countDigits (word) {
+    let sana = 0
+    for(let i = 0; i < word.length; i++) {
+        if(!isNaN(word[i])){
+            sana++;
+        }
+    }
+    return sana;
+
+}
+
+console.log(countDigits("g3s6dje8xt6ga4bv13"))
+
+
+
+function raqamSana (word) {
+    let count = 0;
+    for(let i = 0; i < word.length; i++) {
+        if(!isNaN(word[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(raqamSana("6g3dg36dg23dfg427f6fg7f"));
+
+
+
+function countLetter (word) {
+    let hesob = 0
+    for(let i = 0; i < word.length; i++) {   ////index tekshiryapti
+        if (!isNaN(word[i])) {
+        hesob++;
+    }
+    } 
+    
+    return hesob;
+
+}
+console.log(countLetter("F668tball"))
+
+
+
+
+
+
+
+
+
+// A-TASK bajarildi:
+
+console.log("A-TASK Javoblari")
+
+function counterLetter (letter,word) {
+    let count = 0;
+
+    for(let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+        count++;
+    } 
+  }
+  return count;
+}
+
+console.log(counterLetter("s", "sister"));
+
+
+function harfiniSana (letter,word){
+    let sana = 0;
+
+    for(let i = 0; i < word.length; i++)
+        if (word[i] === letter) {
+            sana++;
+        }
+        return sana;
+} 
+
+console.log(harfiniSana("a","banana"));
+
+function meniSana (letter,word) {
+    let sana = 0
+    for(let i = 0; i < word.length; i++);
+    if (word[i] === letter) {
+        sana++
+    }
+    return sana;
+}
+console.log (harfiniSana("e", "interesting"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // console.log ("Jack Ma maslahtlari");
 // const list = [
 //     "yaxshi talaba boling", // 0-20
@@ -34,15 +143,15 @@
 
 // ASYNC function
 
-console.log ("Jack Ma maslahtlari");
-const list = [
-    "yaxshi talaba boling", // 0-20
-    "togri boshliq tanlang va koproq hato qiling", // 20-30
-    "uzingizga ishlashni boshlang", // 30-40
-    "siz kuchliy bolgan narsalarni qiling", // 40-50
-    "yoshlarga invertitsiya qiling", // 50-60
-    "endi dam oling foydasi yoq", // 60
-];
+// console.log ("Jack Ma maslahtlari");
+// const list = [
+//     "yaxshi talaba boling", // 0-20
+//     "togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "uzingizga ishlashni boshlang", // 30-40
+//     "siz kuchliy bolgan narsalarni qiling", // 40-50
+//     "yoshlarga invertitsiya qiling", // 50-60
+//     "endi dam oling foydasi yoq", // 60
+// ];
 
 // setInterval asynchronousda ishlamaydi.
 /* async function maslahatBering (a) {
@@ -95,65 +204,24 @@ run()*/
 
 // setIntervalni callbackda ishlaydi
 
-/*function maslahatBering (a, callback) {
-    if (typeof a !== 'number') callback("insert a number",null);
-    else if (a <= 20) callback(null, list[0]);
-    else if (a > 20 && a <= 30) callback (null, list[1]);
-    else if (a > 30 && a <= 40) callback (null, list[2]);
-    else if (a > 40 && a <= 50) callback (null, list[3]);
-    else if (a > 50 && a <= 60) callback (null, list[4]);
-    else {
-        setInterval ( function ()  {
-            callback(null, list[5]);
-        }, 1000)
+// function maslahatBering (a, callback) {
+//     if (typeof a !== 'number') callback("insert a number",null);
+//     else if (a <= 20) callback(null, list[0]);
+//     else if (a > 20 && a <= 30) callback (null, list[1]);
+//     else if (a > 30 && a <= 40) callback (null, list[2]);
+//     else if (a > 40 && a <= 50) callback (null, list[3]);
+//     else if (a > 50 && a <= 60) callback (null, list[4]);
+//     else {
+//         setInterval ( function ()  {
+//             callback(null, list[5]);
+//         }, 1000)
         
-    } 
-}
+//     } 
+// }
 
-console.log('passed here 0');
-maslahatBering(65 , (err, data) => {
-    if (err) console.log ("ERROR:", err)
-        else console.log ("Javob:",data)
-});
-console.log('passed here 1')*/
-
-
-
-
-
-
-function counterLetter (letter,word) {
-    let count = 0;
-
-    for(let i = 0; i < word.length; i++) {
-    if (word[i] === letter) {
-        count++;
-    } 
-  }
-  return count;
-}
-
-console.log(counterLetter("s", "sister"));
-
-
-function harfiniSana (letter,word){
-    let sana = 0;
-
-    for(let i = 0; i < word.length; i++)
-        if (word[i] === letter) {
-            sana++;
-        }
-        return sana;
-} 
-
-console.log(harfiniSana("a","banana"));
-
-function meniSana (letter,word) {
-    let sana;
-    for(let i = 0; i < word.length; i++);
-    if (word[i] === letter) {
-        sana++
-    }
-    return sana;
-}
-console.log (harfiniSana("e", "interesting"))
+// console.log('passed here 0');
+// maslahatBering(65 , (err, data) => {
+//     if (err) console.log ("ERROR:", err)
+//         else console.log ("Javob:",data)
+// });
+// console.log('passed here 1')
